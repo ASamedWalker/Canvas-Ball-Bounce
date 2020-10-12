cdb91c70d27a11e3d102ef8cc884683e21b10f24
Abdul Samed Walker <samedwalker50@gmail.com>
2020-10-12 19:54:49 +0000

    #Drawing additional circle on the canvas.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Drawing a circle on a canvas 
+// var canvas = document.getElementById("myCanvas");
+// var context = canvas.getContext("2d");
+
+// context.fillStyle = "#0000ff";
+// context.beginPath();
+// context.arc(100, 100, 50, 0, 2*Math.PI);
+// context.fill();
+
+//Creating a function to draw additional circle in the canvas
+
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#0000ff";
-context.beginPath();
-context.arc(100, 100, 50, 0, 2*Math.PI);
-context.fill();
+function drawBall(x, y, r, color) {
+  context.fillStyle = color;
+  context.beginPath();
+  context.arc(x,y,r,0,2*Math.PI);
+  context.fill();
+}
+
+drawBall(50,50,30,"#ff0000");
+drawBall(120,50,30,"#00ff00");
+drawBall(190,50,30,"#0000ff");
